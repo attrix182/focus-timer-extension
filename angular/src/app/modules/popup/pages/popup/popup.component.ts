d1856fccdc9b4e71b1ed97f492ef8aa652c48ddf
Luciano S <luciano.sinisterra@lamercantil.com.ar>
2023-08-03 13:15:32 -0300

    feat(focus-timer): opcion 1

--- angular/src/app/modules/popup/pages/popup/popup.component.ts
+++ angular/src/app/modules/popup/pages/popup/popup.component.ts
@@ -1,35 +1,37 @@
-import { Component, Inject } from '@angular/core';
-import { bindCallback } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Component, Inject, OnInit } from '@angular/core';
 import { TAB_ID } from '../../../../providers/tab-id.provider';
+import { bindCallback, map } from 'rxjs';
 
 @Component({
   selector: 'app-popup',
   templateUrl: 'popup.component.html',
   styleUrls: ['popup.component.scss']
 })
-export class PopupComponent {
-  time: any;
+export class PopupComponent implements OnInit {
+  duration: number;
+  eta: string = '';
 
   constructor(@Inject(TAB_ID) readonly tabId: number) {}
 
-  async stopTimer(): Promise<void> {
-    await bindCallback<any, any>(chrome.tabs.sendMessage.bind(this, this.tabId, { stop: true }))()
-      .pipe(
-        map((msg) =>
-          chrome.runtime.lastError ? 'The current page is protected by the browser, goto: JIRA and try again.' : msg
-        )
-      )
-      .toPromise();
+  ngOnInit(): void {
+    this.detectChangeTab();
   }
 
-  async startTimer(duration): Promise<void> {
-    await bindCallback<any, any>(chrome.tabs.sendMessage.bind(this, this.tabId, { duration }))()
-      .pipe(
-        map((msg) =>
-          chrome.runtime.lastError ? 'The current page is protected by the browser, goto: JIRA and try again.' : msg
-        )
-      )
-      .toPromise();
+  async detectChangeTab(): Promise<void> {
+    const tab = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
+    this.sendChangeTab(tab[0].id);
+  }
+
+  sendChangeTab(tabID){
+    chrome.tabs.sendMessage(tabID, { tabChanged: true });
+  }
+
+  async startTimer(duration: number, tabId = this.tabId): Promise<void> {
+    this.duration = duration;
+    chrome.tabs.sendMessage(tabId, { duration });
+  }
+
+  async stopTimer(): Promise<void> {
+    chrome.tabs.sendMessage(this.tabId, { stop: true });
   }
 }
--- chrome/src/contentPage.ts
+++ chrome/src/contentPage.ts
@@ -1,39 +1,88 @@
 let time = '';
+let intervalId;
 
 chrome.runtime.onMessage.addListener((request, sender, respond) => {
-  let timeBox = document.createElement('div');
-  timeBox.style.cssText =
-    'position: absolute; top: 10px; right: 15px; z-index: 1000; font-size: 20px; color: white; font-weight: bold; background: #4f6ae3;padding: 10px;border-radius: 15px;';
-  document.body.appendChild(timeBox);
-  let duration = request.duration;
-
-  if (request.stop) {
-    console.log('stop');
-    stopTimer(timeBox);
+  let timeBox = document.getElementById('timeBox');
+  if (!timeBox) {
+    timeBox = document.createElement('div');
+    timeBox.setAttribute('id', 'timeBox');
+    timeBox.style.cssText =
+      'position: fixed; top: 10px; right: 15px; z-index: 1001; font-size: 20px; color: white; font-weight: bold; background: #4f6ae3;padding: 10px;border-radius: 15px;';
+    document.body.appendChild(timeBox);
   }
+
+  let duration = request.duration;
   if (request && request.duration) {
-    startTimer(timeBox, duration);
+    stopTimer(timeBox);
+    timeBox.style.display = 'block';
+    intervalId = startTimer(timeBox, duration - 1);
+  }
+
+  if (request && request.tabChanged) {
+    let remainingTime = getElapsedTime();
+
+    let minutes = remainingTime!.remainingMinutes;
+    let seconds = remainingTime!.remainingSeconds;
+
+    if (minutes == -1 || seconds == -1) {
+      stopTimer(timeBox);
+      return;
+    }
+    console.log('Retomo con: ' + minutes + ':' + seconds);
+    restartTimer(timeBox, minutes + 1, seconds);
+  }
+
+  if (request && request.stop) {
+    stopTimer(timeBox);
   }
 });
 
-function stopTimer(timeBox) {
-  timeBox.style.display = 'none';
-  document.body.removeChild(timeBox);
+function getElapsedTime() {
+  const timerInfo = JSON.parse(localStorage.getItem('timerInfo') || '{}');
+  const { startedAt, duration } = timerInfo;
+  console.log(timerInfo);
+  if (!startedAt || !duration) {
+    return { remainingMinutes: -1, remainingSeconds: -1 };
+  }
+
+  const now = new Date();
+  const startedTime = new Date(startedAt);
+  const elapsedSeconds = (now.getTime() - startedTime.getTime()) / 1000;
+  const remainingMinutes = Math.floor((duration * 60 - elapsedSeconds) / 60);
+  const remainingSeconds = Math.round((duration * 60 - elapsedSeconds) % 60);
+
+  console.log('Remaining: ' + remainingMinutes + ':' + remainingSeconds);
+  return {
+    remainingMinutes: isNaN(remainingMinutes) ? -1 : remainingMinutes,
+    remainingSeconds: isNaN(remainingSeconds) ? -1 : remainingSeconds
+  };
 }
 
-function startTimer(element, duration) {
-  var minute = duration - 1;
-  var sec = 60;
-  setInterval(function () {
+
+const restartTimer = (element, minutes, seconds = 60) => {
+  timer(element, minutes, seconds);
+}
+
+const startTimer = (element, minutes, seconds = 60) => {
+  localStorage.setItem('timerInfo', JSON.stringify({ startedAt: new Date().toISOString(), duration: minutes }));
+  timer(element, minutes, seconds);
+};
+
+
+function timer(element ,minutes, seconds = 60){
+  var minute = minutes;
+  var sec = seconds;
+
+  // Devolver el ID del intervalo para poder limpiarlo si es necesario
+  return setInterval(() => {
     sec--;
     if (sec.toString().length == 1) {
       time = minute + ':0' + sec;
       element.innerHTML = minute + ':0' + sec;
     } else {
       time = minute + ':' + sec;
-      element.innerHTML = minute + ':' + sec;
+      element.innerHTML = time;
     }
-
     if (sec == 0) {
       minute--;
       sec = 60;
@@ -44,3 +93,10 @@ function startTimer(element, duration) {
     }
   }, 1000);
 }
+
+// Función para detener el temporizador
+const stopTimer = (timeBox) => {
+  clearInterval(intervalId);
+  localStorage.removeItem('timerInfo');
+  timeBox.style.display = 'none';
+};
